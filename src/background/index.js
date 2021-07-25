d0e21cf3d2073d4f0ff20e043d74086ac8386103
sarmat <skhachirov12@gmail.com>
2021-07-25 12:49:35 +0300

    chore: rebuild structure — works for developing, but still WIP for prod

--- src/background/index.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-undef */
-if (process.env.NODE_ENV === 'development') {
-  chrome.runtime.onInstalled.addListener(() => {
-    chrome.tabs.create({
-      url: 'popup/index.html',
-    })
-  })
-}
