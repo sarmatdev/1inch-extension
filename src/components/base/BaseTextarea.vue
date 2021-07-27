04d653ea77b7109e135d3310ead6d6fb551b874f
sarmat <skhachirov12@gmail.com>
2021-07-27 16:21:35 +0300

    feat: base textarea

---
+++ src/components/base/BaseTextarea.vue
@@ -0,0 +1,36 @@
+<template>
+  <textarea
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+    class="base-textarea"
+    type="text"
+    :placeholder="placeholder"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'BaseTextarea',
+  props: {
+    modelValue: [String, Number],
+    placeholder: String
+  }
+})
+</script>
+
+<style lang="scss">
+.base-textarea {
+  background-color: #06070a;
+  border: 1px solid #37445a;
+  color: #fff;
+  padding: 10px 20px;
+  outline: none;
+  border-radius: 12px;
+  font-weight: 500;
+  width: 100%;
+  min-height: 200px;
+  resize: none;
+}
+</style>
