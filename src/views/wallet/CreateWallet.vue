a6cef3ba75ec2e43518e0a52b1fd40cf9b0577e9
sarmat <skhachirov12@gmail.com>
2021-07-27 16:23:19 +0300

    feat[wip]: implement wallet logic

--- src/views/wallet/CreateWallet.vue
+++ src/views/wallet/CreateWallet.vue
@@ -11,6 +11,7 @@
       text="Never give your recovery phrase to third parties, keep it in a safe place!"
     ></base-warning>
     <WordList :words="mnemonic" />
+    <p>{{ wallet.mnemonic.phrase }}</p>
   </section>
   <section class="agreement">
     <div>
@@ -22,12 +23,32 @@
     </div>
     <base-button :disabled="!agree" class="w-full">Continue</base-button>
   </section>
+  <section>
+    <h2 class="font-bold">Confirm mnemonic</h2>
+    <span>Set the wallet name</span>
+    <base-input
+      v-model="walletName"
+      class="w-full mb-5"
+      placeholder="Name"
+    ></base-input>
+    <base-textarea v-model.trim="mnemonicForConfirm"></base-textarea>
+    <div>
+      {{ mnemonicConfirmed }}
+    </div>
+    <base-button
+      @click="saveWallet"
+      :disabled="!mnemonicConfirmed"
+      class="w-full"
+      >Create Wallet</base-button
+    >
+  </section>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
-import { ethers } from 'ethers'
+import { useStore } from 'vuex'
 import WordList from '@/components/common/WordList.vue'
+import { createRandom, validateMnemonic } from '@/services/account'
 
 export default defineComponent({
   name: 'Create wallet',
@@ -35,14 +56,44 @@ export default defineComponent({
     WordList
   },
   setup() {
-    const phrase = ethers.Wallet.createRandom().mnemonic
-    const mnemonic = phrase.phrase.split(' ')
+    const store = useStore()
+    const wallet = createRandom()
+    const mnemonic = wallet.mnemonic.phrase.split(' ')
+
+    console.log('createRandom', wallet)
+    console.log(
+      'validateMnemonic',
+      '0x49D4710499CD8121D1022CeB77026a7eC8CdE6e2',
+      validateMnemonic(
+        'cousin loop token junk trumpet then guitar vague choice license south reunion'
+      )
+    )
 
     const agree = ref(false)
+    const walletName = ref('')
+    const mnemonicForConfirm = ref('')
 
     return {
+      store,
       mnemonic,
-      agree
+      agree,
+      wallet,
+      walletName,
+      mnemonicForConfirm
+    }
+  },
+  computed: {
+    mnemonicConfirmed() {
+      // @ts-ignore
+      return validateMnemonic(this.mnemonicForConfirm)
+    }
+  },
+  methods: {
+    saveWallet() {
+      this.store.dispatch('wallets/storeWallet', {
+        name: this.walletName,
+        ...this.wallet
+      })
     }
   }
 })
