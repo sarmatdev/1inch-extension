5618adfe1b7f10dc3bc6fdd8183bf03df6d94dfd
sarmat <skhachirov12@gmail.com>
2021-07-29 13:06:00 +0300

    refactoring: decompose structure

--- src/components/common/CreateWallet/ConfirmMnemonic.vue
+++ src/components/common/CreateWallet/ConfirmMnemonic.vue
@@ -1,13 +1,70 @@
 <template>
-  <h1>Confirm Mnemonic</h1>
+  <h1>Confirm mnemonic</h1>
+  <section>
+    <base-input
+      v-model="walletName"
+      class="w-full mb-5"
+      placeholder="Name"
+      label="Set the wallet name"
+    ></base-input>
+    <base-textarea
+      label="You must write the phrase in the correct order"
+      v-model.trim="mnemonicForConfirm"
+    ></base-textarea>
+    {{ walletFromMnemonic }}
+    <base-button
+      @click="saveWallet"
+      :disabled="!mnemonicConfirmed"
+      class="w-full"
+      >Create Wallet</base-button
+    >
+  </section>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-
+import { defineComponent, ref, computed } from 'vue'
+import { useStore } from 'vuex'
+import { useRouter } from 'vue-router'
+import { validateMnemonic } from '@/services/account'
 export default defineComponent({
-  name: 'ConfirmMnemonic'
+  name: 'ConfirmMnemonic',
+  setup() {
+    const store = useStore()
+    const router = useRouter()
+    const walletName = ref('')
+    const mnemonicForConfirm = ref(
+      'life equip reform inch author human lizard spray huge capable volume tenant'
+    )
+
+    const mnemonicConfirmed = computed(() => {
+      return validateMnemonic(mnemonicForConfirm)
+    })
+
+    // const walletFromMnemonic = computed(() => {
+    //   return createFromMnemonic(mnemonicForConfirm.value)
+    // })
+
+    function saveWallet() {
+      store.dispatch('wallets/storeWallet', {
+        name: walletName.value,
+        address: mnemonicConfirmed.value.address,
+        privateKey: mnemonicConfirmed.value.privateKey
+      })
+
+      router.push('/')
+    }
+
+    return {
+      store,
+      router,
+      walletName,
+      mnemonicForConfirm,
+      mnemonicConfirmed,
+      saveWallet
+    }
+  }
 })
+// life equip reform inch author human lizard spray huge capable volume tenant
 </script>
 
 <style></style>
--- src/views/wallet/CreateWallet.vue
+++ src/views/wallet/CreateWallet.vue
@@ -1,99 +1,42 @@
 <template>
-  <h1>Create Wallet</h1>
-  <section class="mnemonic">
-    <h1 class="text-white font-black text-lg">Your mnemonic phrase</h1>
-    <p>
-      Write down or copy this phrase in the correct order and keep it in a safe
-      place.
-    </p>
-    <base-warning
-      class="mt-1"
-      text="Never give your recovery phrase to third parties, keep it in a safe place!"
-    ></base-warning>
-    <WordList :words="mnemonic" />
-    <p>{{ wallet.mnemonic.phrase }}</p>
-  </section>
-  <section class="agreement">
-    <div>
-      <input type="checkbox" id="creation-agreement" v-model="agree" />
-      <label for="creation-agreement"
-        >I understand that if you lose your recovery phrase, you will not be
-        able to access my funds</label
-      >
-    </div>
-    <base-button :disabled="!agree" class="w-full">Continue</base-button>
-  </section>
-  <section>
-    <h2 class="font-bold">Confirm mnemonic</h2>
-    <span>Set the wallet name</span>
-    <base-input
-      v-model="walletName"
-      class="w-full mb-5"
-      placeholder="Name"
-    ></base-input>
-    <base-textarea v-model.trim="mnemonicForConfirm"></base-textarea>
-    <div>
-      {{ mnemonicConfirmed }}
-    </div>
-    <base-button
-      @click="saveWallet"
-      :disabled="!mnemonicConfirmed"
-      class="w-full"
-      >Create Wallet</base-button
-    >
-  </section>
+  <component @nextStep="nextStep" :is="currentStep"> </component>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
-import { useStore } from 'vuex'
+import { computed, defineComponent, ref } from 'vue'
 import WordList from '@/components/common/WordList.vue'
-import { createRandom, validateMnemonic } from '@/services/account'
+import CreateMnemonic from '@/components/common/CreateWallet/CreateMnemonic.vue'
+import ConfirmMnemonic from '@/components/common/CreateWallet/ConfirmMnemonic.vue'
 
 export default defineComponent({
   name: 'Create wallet',
   components: {
-    WordList
+    WordList,
+    ConfirmMnemonic,
+    CreateMnemonic
   },
   setup() {
-    const store = useStore()
-    const wallet = createRandom()
-    const mnemonic = wallet.mnemonic.phrase.split(' ')
+    const step = ref(1)
 
-    console.log('createRandom', wallet)
-    console.log(
-      'validateMnemonic',
-      '0x49D4710499CD8121D1022CeB77026a7eC8CdE6e2',
-      validateMnemonic(
-        'cousin loop token junk trumpet then guitar vague choice license south reunion'
-      )
+    const currentStep = computed(() =>
+      step.value === 1 ? 'CreateMnemonic' : 'ConfirmMnemonic'
     )
 
     const agree = ref(false)
     const walletName = ref('')
     const mnemonicForConfirm = ref('')
 
+    function nextStep(value) {
+      step.value = value
+    }
+
     return {
-      store,
-      mnemonic,
+      step,
       agree,
-      wallet,
       walletName,
-      mnemonicForConfirm
-    }
-  },
-  computed: {
-    mnemonicConfirmed() {
-      // @ts-ignore
-      return validateMnemonic(this.mnemonicForConfirm)
-    }
-  },
-  methods: {
-    saveWallet() {
-      this.store.dispatch('wallets/storeWallet', {
-        name: this.walletName,
-        ...this.wallet
-      })
+      nextStep,
+      mnemonicForConfirm,
+      currentStep
     }
   }
 })
