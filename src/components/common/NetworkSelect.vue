01ea2dcaf8c8fb79b048f502dd7d9362edaff9f4
AlanNabiev <alannabiev2001@gmail.com>
2021-07-30 07:18:33 +0300

    feat:add NetworkSelect component

---
+++ src/components/common/NetworkSelect.vue
@@ -0,0 +1,132 @@
+<template>
+  <Listbox v-model="selectedNetwork" v-slot="{ open }">
+    <div class="network__select">
+      <ListboxButton
+        class="network__select__button"
+        :class="[
+          {
+            'network__select__button--eth__active':
+              selectedNetwork.name === 'Etherium'
+          },
+          {
+            'network__select__button--bscm__active':
+              selectedNetwork.name === 'BSC Mainnet'
+          },
+          {
+            'network__select__button--polygon__active':
+              selectedNetwork.name === 'Polygon Network'
+          }
+        ]"
+      >
+        <base-icon
+          class="network__select--network__icon"
+          :icon="`/network-select${selectedNetwork.icon}-selected`"
+        ></base-icon>
+        <base-title>{{ selectedNetwork.name }}</base-title>
+        <base-icon
+          class="network__select__button--btn__icon"
+          :class="{ 'transform rotate-180': open }"
+          icon="/network-select/arrow"
+        ></base-icon>
+      </ListboxButton>
+
+      <transition
+        leave-active-class="transition duration-100 ease-in"
+        leave-from-class="opacity-100"
+        leave-to-class="opacity-0"
+      >
+        <ListboxOptions class="network__select__options">
+          <ListboxOption
+            v-slot="{ active }"
+            v-for="network in networks"
+            :key="network.name"
+            :value="network"
+            as="template"
+          >
+            <li
+              :class="[
+                active ? ' bg-gray-800 text-word-3' : 'bg-gray-700 text-word-4',
+                'network__select__options__item '
+              ]"
+            >
+              <base-icon
+                class="network__select--network__icon"
+                :icon="`/network-select${network.icon}`"
+              ></base-icon>
+              <base-title class="network__select__options__item--name">{{
+                network.name
+              }}</base-title>
+            </li>
+          </ListboxOption>
+        </ListboxOptions>
+      </transition>
+    </div>
+  </Listbox>
+</template>
+
+<script>
+import { ref } from 'vue'
+import {
+  Listbox,
+  ListboxButton,
+  ListboxOptions,
+  ListboxOption
+} from '@headlessui/vue'
+
+export default {
+  components: {
+    Listbox,
+    ListboxButton,
+    ListboxOptions,
+    ListboxOption
+  },
+
+  setup() {
+    const networks = [
+      { name: 'Etherium', icon: '/eth' },
+      { name: 'BSC Mainnet', icon: '/bsc' },
+      { name: 'Polygon Network', icon: '/polygon' }
+    ]
+    const selectedNetwork = ref(networks[0])
+
+    return {
+      networks,
+      selectedNetwork
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.network__select {
+  @apply relative mt-1 z-40 text-sm;
+  &--network__icon {
+    @apply w-8 h-8;
+    background-color: transparent;
+  }
+  &__button {
+    @apply px-3.5 py-2.5 rounded-2xl w-full flex justify-between items-center text-white;
+    &--eth__active {
+      background: linear-gradient(73.28deg, #495bfc 6.51%, #114188 88.45%);
+    }
+    &--bscm__active {
+      background: linear-gradient(73.28deg, #403c3c 6.51%, #403721 88.45%);
+    }
+    &--polygon__active {
+      background: linear-gradient(73.28deg, #8247e5 6.51%, #6027c0 88.45%);
+    }
+    &--btn__icon {
+      @apply text-white ml-2.5 w-4 h-4;
+    }
+  }
+  &__options {
+    @apply absolute w-full py-1 mt-1 focus:outline-none rounded-2xl bg-gray-700;
+    &__item {
+      @apply flex items-center cursor-default select-none my-2 px-5 py-2;
+      &--name {
+        @apply ml-2.5 text-left;
+      }
+    }
+  }
+}
+</style>
