86964e3b62be84162ab1a29f89b0716f3dd497db
sarmat <skhachirov12@gmail.com>
2021-08-08 09:54:52 +0300

    feat: toHex

--- src/utils/eth-utils.ts
+++ src/utils/eth-utils.ts
@@ -3,3 +3,7 @@ import { utils } from 'ethers'
 export function isAddress(address: string): boolean {
   return utils.isAddress(address)
 }
+
+export function toHex(value: string) {
+  return `0x${Number(value).toString(16)}`
+}
